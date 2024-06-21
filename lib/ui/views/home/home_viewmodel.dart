086ca3a4668aeff6396f49ec94e41ce8c8424a5e
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2024-06-21 12:45:15 +0100

    :construction: chore: App Utils Setup

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -1,13 +1,6 @@
-import 'package:calpal/app/app.bottomsheets.dart';
-import 'package:calpal/app/app.dialogs.dart';
-import 'package:calpal/app/app.locator.dart';
-import 'package:calpal/ui/common/app_strings.dart';
 import 'package:stacked/stacked.dart';
-import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
-  final _bottomSheetService = locator<BottomSheetService>();
 
   String get counterLabel => 'Counter is: $_counter';
 
@@ -18,19 +11,4 @@ class HomeViewModel extends BaseViewModel {
     rebuildUi();
   }
 
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Stacked Rocks!',
-      description: 'Give stacked $_counter stars on Github',
-    );
-  }
-
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: ksHomeBottomSheetTitle,
-      description: ksHomeBottomSheetDescription,
-    );
-  }
 }
